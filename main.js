40676bea9909c3359bd229fad7bc84a0cbe55766
wozacosta <rabah.m.samy@gmail.com>
2016-11-24 14:58:14 +0000

    Major update

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+$('#learning1').progress({
+  percent: 22
+});
+
+$('#learning2').progress({
+  percent: 17
+});
+
+$('#learning3').progress({
+  percent: 24
+});
+
+$('#learning4').progress({
+  percent: 51
+});
+
+$('#learning5').progress({
+  percent: 75
+});
